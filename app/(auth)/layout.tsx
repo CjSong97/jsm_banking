421ee5c6cee3aa89886f27888bff3c15c8547cec
CjSong97 <songcj2010@gmail.com>
2024-10-27 14:05:40 +0000

    Integrate appwrite and sentry, implement auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <main>
+      <main className="flex min-h-screen w-full justify-between font-inter">
           {children}
+          <div className="auth-asset">
+            <div>
+              {/* TODO: Re-capture a new screenshot of the homescreen and attach as image */}
+              <Image src="/icons/auth-image.svg" alt="auth image"
+              width={500}
+              height={500}
+              />
+            </div>
+          </div>
       </main>
     );
   }
